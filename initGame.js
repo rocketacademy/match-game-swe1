230c53c611891d0019e1029d1d10d3bffb1a8898
ZAF07 <zaffere07@gmail.com>
2021-04-14 22:21:17 +0800

    Added interval, player name and reset button functionality

--- initGame.js
+++ initGame.js
@@ -14,10 +14,10 @@ const initGame = () => {
   // create this special deck by getting the doubled cards and
   // making a smaller array that is ( boardSize squared ) number of cards
   let doubleDeck = makeDeck();
-  console.log("THIS IS DOUBLEDECK ---> ", doubleDeck);
+  // console.log("THIS IS DOUBLEDECK ---> ", doubleDeck);
   // Slice method returns a copy of the array with the selected elements
   let deckSubset = doubleDeck.slice(0, boardSize * boardSize);
-  console.log("THIS IS DECKSUBSET ---> ", deckSubset);
+  // console.log("THIS IS DECKSUBSET ---> ", deckSubset);
   deck = shuffleCards(deckSubset);
 
   // deal the cards out to the board data structure
